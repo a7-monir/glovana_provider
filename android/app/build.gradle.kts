fa587b96ae86412a67296ec5a97c80f60b474ee9
a7-monir <a7mdsaleh10@gmail.com>
2025-11-16 22:41:05 +0200

    edit

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "com.alianCode.glovana_provider"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "6.0.0"
+        versionCode = 7
+        versionName = "7.0.0"
     }
 
     signingConfigs {
